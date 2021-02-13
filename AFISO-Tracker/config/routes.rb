389cb4688d74e723378bb3a53f370add4db630cd
BrentonLenzen <45884884+BrentonLenzen@users.noreply.github.com>
2021-02-13 16:25:03 -0600

    Updated routes for members and semesters

--- AFISO-Tracker/config/routes.rb
+++ AFISO-Tracker/config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :semesters do
+	member do
+		get :delete
+	end
+  end
+
+  resources :members do
+	member do
+		get :delete
+	end
+  end
 end
