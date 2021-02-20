56c16488ff75c2df5644529a744d6f3a26044ec4
Anuj Parakh <anuj@anujinfotech.com>
2021-02-20 14:22:24 -0600

    added password page

--- AFISO-Tracker/app/controllers/application_controller.rb
+++ AFISO-Tracker/app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+    skip_before_action :check_for_lockup, raise: false
+
 end
--- AFISO-Tracker/config/routes.rb
+++ AFISO-Tracker/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  mount Lockup::Engine, at: '/lockup'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
