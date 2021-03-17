54ce63c8e47028699df7173926cbccac7956ef94
BrentonLenzen <45884884+BrentonLenzen@users.noreply.github.com>
2021-03-17 11:51:29 -0500

    Added helper functions for mailing list / viewing active members

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,5 +2,31 @@ class Member < ApplicationRecord
     has_many :payments
     validates_presence_of :name, :email
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-    validates_uniqueness_of :email
+	
+	def self.get_active_in_semester(s_id)
+	  # look at payments for given semester
+	  @payments = Payment.where(semester_id: s_id)
+	  m_ids = []
+	  @payments.each_with_index do |payment, index|
+	    m_ids.push(payment.member_id)
+	  end
+	  # grab members who made payments in given semester
+	  @members = []
+	  m_ids.each_with_index do |id, index|
+		@members.push(Member.find(id))
+	  end
+	  return @members
+	end
+end
+
+# from a list of members, generate a list of their emails
+def generate_mailing_list(members)
+  @mailing_list = ""
+  members.each_with_index do |member, index|
+    @mailing_list += member.email
+    if index != members.size - 1
+      @mailing_list += ","
+	end
+  end
+  return @mailing_list
 end
