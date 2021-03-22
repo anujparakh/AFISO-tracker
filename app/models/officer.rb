c0bc51be04598d38d56c0d8b07e45b66de343fc3
Anuj Parakh <anuj@anujinfotech.com>
2021-03-21 22:29:30 -0500

    Dev (#23)
    * fixed navbar collapse problems
    
    * Added dues report
    
    * Done with safely deleting and also viewing active member semesters (#22)
    
    Co-authored-by: Phelan27 <phelan27@tamu.edu>
    Co-authored-by: Amg9973 <61438983+Amg9973@users.noreply.github.com>
    Co-authored-by: Justin Lee <43049757+jNLeee@users.noreply.github.com>

--- app/models/officer.rb
+++ app/models/officer.rb
@@ -1,5 +1,5 @@
 class Officer < ApplicationRecord
-    has_many :payments
-    has_many :transactions
+    has_many :payments, dependent: :destroy
+    has_many :transactions, dependent: :destroy
     validates_uniqueness_of :email
 end
