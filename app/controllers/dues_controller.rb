08c855025fa5bec643b32dfca69b20703094ece2
BrentonLenzen <45884884+BrentonLenzen@users.noreply.github.com>
2021-04-03 01:18:55 -0500

    Updated files to change "Payments" to "Dues"

---
+++ app/controllers/dues_controller.rb
@@ -0,0 +1,108 @@
+class DuesController < ApplicationController
+  # before_action :check_for_lockup
+
+  ### READ ###
+  def index
+    @selectedSemester = params[:semester]
+    @selectedSemester = "All" if @selectedSemester == nil
+
+    @payments = Due.filter_on_semester(@selectedSemester)
+
+    @total = Due.get_total(@payments)
+  end
+
+  def show
+    @payment = Due.find(params[:id])
+  end
+
+  ### CREATE ###
+  def new
+    @payment = Due.new
+    @members = Member.order("name ASC")
+    @semesters = Semester.order("start_date DESC")
+    @officers = Officer.order("name ASC")
+
+    @payment.payment_date = DateTime.now
+    if params[:member_id] != nil
+      @payment.member_id = params[:member_id]
+    end
+
+  end
+
+  def create
+    @members = Member.order("name ASC")
+    @semesters = Semester.order("start_date DESC")
+    @officers = Officer.order("name ASC")
+
+    @payment = Due.new(due_params)
+
+    @payment.payment_date = DateTime.now
+
+    # Make sure associated member, officer, and semester exist
+    if valid_relations(@payment)
+      if @payment.save
+        flash[:notice] = "Payment Successfully Created!"
+        redirect_to(dues_path)
+      else
+        flash[:errors] = "Invalid fields"
+        render("new")
+      end
+    else
+      flash[:errors] = "Invalid officer, member or semester"
+      render("new")
+    end
+  end
+
+  ### UPDATE ###
+  def edit
+    @payment = Due.find(params[:id])
+    @members = Member.order("name ASC")
+    @semesters = Semester.order("start_date DESC")
+    @officers = Officer.order("name ASC")
+  end
+
+  def update
+    @payment = Due.find(params[:id])
+    @members = Member.order("name ASC")
+    @semesters = Semester.order("start_date DESC")
+    @officers = Officer.order("name ASC")
+
+    if @payment.update(due_params)
+      flash[:notice] = "Payment Successfully Updated!"
+      redirect_to(dues_path)
+    else
+      flash[:errors] = "Invalid fields"
+      render("edit")
+    end
+  end
+
+  ### DELETE ###
+  def delete
+    @payment = Due.find(params[:id])
+  end
+
+  def destroy
+    @payment = Due.find(params[:id])
+    @payment.destroy
+    flash[:notice] = "Payment has been deleted"
+    redirect_to(dues_path)
+  end
+
+  private
+
+  def due_params
+    params.require(:due).permit(:member_id, :semester_id, :officer_id, :payment_amount)
+  end
+
+  def valid_relations(form_record)
+    semester_exists = Semester.exists?(id: form_record.semester_id)
+    officer_exists = Officer.exists?(id: form_record.officer_id)
+    member_exists = Member.exists?(id: form_record.member_id)
+
+    if semester_exists && officer_exists && member_exists
+      true
+    else
+      false
+    end
+  end
+end
---
+++ spec/models/due_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe Due, :type => :model do
+    subject {
+        described_class.new(payment_amount: 100, payment_date: DateTime.now)
+    }
+
+    describe "Validations" do
+        it "is not valid without member, officer and semester" do
+            expect(subject).to_not be_valid
+        end
+
+        it "is not valid without an amount" do
+            subject.payment_amount = nil
+            expect(subject).to_not be_valid
+        end
+    end
+
+    describe "Test Methods" do
+        it "filter on a semester" do
+            expect(Due.filter_on_semester(Semester.last.id).size).to be >= 0
+        end
+
+        it "filter on all semesters" do
+            expect(Due.filter_on_semester("All").size).to be >= 0
+        end
+    end
+end
