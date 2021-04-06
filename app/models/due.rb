858640bcb006e92274d0ce321912b82b99972cad
BrentonLenzen <45884884+BrentonLenzen@users.noreply.github.com>
2021-04-05 23:07:40 -0500

    Changed payment.rb to due.rb

--- app/models/due.rb
+++ app/models/due.rb
@@ -1,17 +1,18 @@
-class Payment < ApplicationRecord
+class Due < ApplicationRecord
   belongs_to :member
-  belongs_to :semester
+  belongs_to :semester_1, :class_name => 'Semester', :foreign_key => 'semester_id_1'
+  belongs_to :semester_2, :class_name => 'Semester', :foreign_key => 'semester_id_2', :optional => true
   belongs_to :officer
 
   validates_presence_of :payment_amount
 
   def self.filter_on_semester(semester_id)
-
+	# get any Dues that have the matching semester id
     if semester_id == "All"
-      return Payment.all
+      return Due.all
     else
       @semester = Semester.find(semester_id)
-      return Payment.where({ semester_id: semester_id })
+      return Due.where({ semester_id_1: semester_id }).or(Due.where({ semester_id_2: semester_id }))
     end
 
   end
