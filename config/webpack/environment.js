c0c65a0e30a02af756bd71c07ffaccf308bf522f
Anuj Parakh <anuj@anujinfotech.com>
2021-04-10 01:48:34 -0500

    fixed webpacker stuff

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,3 @@
 const { environment } = require('@rails/webpacker')
-const WebpackDevServer = require('webpack-dev-server')
-
-environment.plugins.append('Provide',
-    new WebpackDevServer.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery',
-        Popper: ['popper.js', 'default']
-    })
-)
 
 module.exports = environment
