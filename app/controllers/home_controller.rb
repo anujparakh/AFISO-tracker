d0dd159e33931a1ecd264ffdfd3dff7d7a3658f0
Anuj Parakh <anuj@anujinfotech.com>
2021-04-09 22:31:00 -0500

    working dark mode!

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,5 +8,14 @@ class HomeController < ApplicationController
   def settings
     @officers = Officer.order('email ASC')
     @officer = Officer.new
+
+    if params[:theme] != nil
+      if params[:theme] == "dark"
+        cookies[:theme] = "dark"
+      else
+        cookies[:theme] = "light"
+      end
+      flash[:notice] = "Changed to " + cookies[:theme] + " mode"
+    end
   end
 end
